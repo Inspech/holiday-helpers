d9b176de73a8f25d06b827542f59ff0c337e0975
Sumiko Yanagi <yanagiryoichi@gmail.com>
2024-12-20 19:08:09 -0800

    Fireplace: Add additional type information

--- BP_PC/scripts/mco/santa/0config/blocks/fireplace.js
+++ BP_PC/scripts/mco/santa/0config/blocks/fireplace.js
@@ -1,4 +1,5 @@
-import { clamp, clampedRandom } from "../../../0utilities/math"
+import { clampedRandom } from "../../../0utilities/math"
+
 
 /** General settings and configurations for the Fireplace block */
 export const fireplaceBlockConfig = {
@@ -26,14 +27,16 @@ export const fireplaceBlockConfig = {
 }
 
 /** This is the array of different items used to fuel a Fireplace block */
-export const firewoodFuelItems = [
-    'minecraft:acacia_log',
-    'minecraft:birch_log',
-    'minecraft:cherry_log',
-    'minecraft:dark_oak_log',
-    'minecraft:jungle_log',
-    'minecraft:mangrove_log',
-    'minecraft:oak_log',
-    'minecraft:pale_oak_log',
-    'minecraft:spruce_log'
-]
+export const firewoodFuelItems = /** @type {const} */ (
+    [
+        'minecraft:acacia_log',
+        'minecraft:birch_log',
+        'minecraft:cherry_log',
+        'minecraft:dark_oak_log',
+        'minecraft:jungle_log',
+        'minecraft:mangrove_log',
+        'minecraft:oak_log',
+        'minecraft:pale_oak_log',
+        'minecraft:spruce_log'
+    ]
+)
